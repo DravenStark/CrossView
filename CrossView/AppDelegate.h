bdbcef539445e46e1a57fb756cfb70d482cb5d20
apple <apple@apple.local>
2014-04-01 20:00:40 +0800

    Initial Commit

---
+++ CrossView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrossView
+//
+//  Created by apple on 14-4-1.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrossViewTests/CrossViewTests.m
@@ -0,0 +1,34 @@
+//
+//  CrossViewTests.m
+//  CrossViewTests
+//
+//  Created by apple on 14-4-1.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CrossViewTests : XCTestCase
+
+@end
+
+@implementation CrossViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
